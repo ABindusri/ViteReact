ec8f43c9e821f7f58a2b65ad927a86b0de917532
ABindusri <atyambindusri@gmail.com>
2024-10-08 21:52:56 +0530

    importing and added

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './App.css'
+import './style.css'
+import Resume from './assets/Bindu_Sri_Atyam_Resume___Frontend.pdf'
+import Photo from './assets/photo.jpg'
+function App() {
+//const [count,setCount]= useState()      /* Hook declaration */
+
+
+  return (
+    <>    
+    { /* wrapper components <></> */} 
+      <img src={Photo} alt="bindu image"></img>  { /* giving the variable that is used in the import to get the image path */} 
+      <p>
+       hi   
+   </p>
+    </>
+  )
+}
+
+export default App
