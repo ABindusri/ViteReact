09d3618cccad8439fcbd9040e084c1ed25465b7b
ABindusri <atyambindusri@gmail.com>
2024-10-08 22:32:55 +0530

    Created both types of components

---
+++ src/components/heading.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class heading extends Component {  // class name extends component name , Component is a builtin class in react package (imports)
+  render() {
+    return (
+      <div>heading</div>
+    )
+  }
+}
