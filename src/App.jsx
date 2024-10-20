de683b6142d755e69df5937359929b992f6c61b1
ABindusri <atyambindusri@gmail.com>
2024-10-20 12:29:38 +0530

    Code Refactoring/Modifying

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
 import React from 'react'
 import './App.css'
 import './style.css'
-import Resume from './assets/Bindu_Sri_Atyam_Resume___Frontend.pdf'
-import Photo from './assets/photo.jpg'
-import FunctionComp from './components/FunctionComp'  // variable name beside the import keyword must be in Pascal case     // FunctionComp is a sub component/child to app.jsx
 import PropComp from './components/PropComp'
-import StateComp from './components/StateComp'
 import CountComp from './components/CountComp'
-import ReactListsComp from './components/ReactListsComp'
 import { useState } from 'react'
-import UserListComp from './components/UserListComp'
 import Form from './components/Form'
 import {Route,Routes, BrowserRouter} from 'react-router-dom' // importing route, BrowserRouter and routes from react router dom , we need to import these three to set routing
 import UserDataComp from './components/UserDataComp'
