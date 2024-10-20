66146dbcd88dc7c35c53ada65aad2aa5931792b1
ABindusri <atyambindusri@gmail.com>
2024-10-20 13:10:41 +0530

    Resolve the counter component issue

--- src/components/CountComp.jsx
+++ src/components/CountComp.jsx
@@ -5,20 +5,20 @@ const CountComp = () => {
 const handleIncCount=()=>
 {
     setCount((prevState)=>{  
-prevState+1
+return prevState+1
     });
 }
 const handleDecCount=()=>
 {
     setCount((prevState)=>{
-        prevState-1
+       return prevState-1
     });
 
 }
   return (
     <div className='flex'>
         <button onClick={handleIncCount}  className='text-xl border-2 p-3 m-3'>+</button>
-        <p className='p-3 m-2'> {count}</p>
+        <p className='p-3 m-2 text-black'> {count}</p>
         <button onClick={handleDecCount} className='text-xl border-2 p-3 m-3'>-</button>
         </div>
   )
